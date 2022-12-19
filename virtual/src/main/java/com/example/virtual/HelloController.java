70c860c5cf7e3aef447cd4dd8fb2272be7577d89
joand <joandyalejogarcia@outlook.com>
2022-12-18 23:01:12 -0500

    create border pane containing bottom text panel -to do----------------- + fix side panel layout + figure out what layout to use

--- virtual/src/main/java/com/example/virtual/HelloController.java
+++ virtual/src/main/java/com/example/virtual/HelloController.java
@@ -6,6 +6,10 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label test1;
+    @FXML
+    private Label test2;
 
     @FXML
     protected void onHelloButtonClick() {
