bff3e9229e5cf91989915cdcea920b52ff979d98
joand <joandyalejogarcia@outlook.com>
2022-12-20 21:31:23 -0500

    Changes by Edison added senderMessage method that sets the label's text to the field's text value

--- virtual/src/main/java/com/example/virtual/HelloController.java
+++ virtual/src/main/java/com/example/virtual/HelloController.java
@@ -3,17 +3,22 @@ package com.example.virtual;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label messageSent;
+    private TextArea sentMessages;
     @FXML
     private TextField messageField;
 
     @FXML
     public void sendMessage() {
-        messageSent.setText(messageField.getText() + "\n");
+        String message;
+
+        message = messageField.getText() + "\n";
         messageField.setText("");
+
+        sentMessages.appendText(message);
     }
 }
