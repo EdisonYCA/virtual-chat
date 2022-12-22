21ce36161e32ba73359d1664b82f53be35ca29f3
EdisonYCA <edisonyca127@gmail.com>
2022-12-21 22:22:33 -0500

    fix sendMessage function to create new VBox for each message that is sent and/or receive and properly aligned them

--- virtual/src/main/java/com/example/virtual/HelloController.java
+++ virtual/src/main/java/com/example/virtual/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.virtual;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
-    private TextArea sentMessages;
+    private VBox messageDisplay;
     @FXML
     private TextField messageField;
 
@@ -19,6 +22,16 @@ public class HelloController {
         message = messageField.getText() + "\n";
         messageField.setText("");
 
-        sentMessages.appendText(message);
+        if (message.equals("sent\n")) {
+            VBox sentMessage = new VBox();
+            sentMessage.setAlignment(Pos.BOTTOM_LEFT);
+            sentMessage.getChildren().add(new Text(message));
+            messageDisplay.getChildren().add(sentMessage);
+        } else if (message.equals("receive\n")) {
+            VBox receiveMessage = new VBox();
+            receiveMessage.setAlignment(Pos.BOTTOM_RIGHT);
+            receiveMessage.getChildren().add(new Text(message));
+            messageDisplay.getChildren().add(receiveMessage);
+        }
     }
 }
