3c5fb43cad72e694b06ad70ba4b0437dd2eb60b8
EdisonYCA <edisonyca127@gmail.com>
2022-12-21 23:03:19 -0500

    TODO: Fix scroll pane

--- virtual/src/main/java/com/example/virtual/HelloController.java
+++ virtual/src/main/java/com/example/virtual/HelloController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -25,13 +27,20 @@ public class HelloController {
         if (message.equals("sent\n")) {
             VBox sentMessage = new VBox();
             sentMessage.setAlignment(Pos.BOTTOM_LEFT);
+            messageDisplay.setAlignment(Pos.BOTTOM_LEFT);
             sentMessage.getChildren().add(new Text(message));
             messageDisplay.getChildren().add(sentMessage);
         } else if (message.equals("receive\n")) {
             VBox receiveMessage = new VBox();
             receiveMessage.setAlignment(Pos.BOTTOM_RIGHT);
+            messageDisplay.setAlignment(Pos.BOTTOM_RIGHT);
             receiveMessage.getChildren().add(new Text(message));
             messageDisplay.getChildren().add(receiveMessage);
         }
+    }
+
+    @FXML
+    public void sendMessageKey(ActionEvent event){
+        sendMessage();
     }
 }
