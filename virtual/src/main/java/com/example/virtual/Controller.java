c0f0ae6a7bba345367ce1b903eff385c724e4007
EdisonYCA <edisonyca127@gmail.com>
2022-12-24 10:28:06 -0500

    Renamed file

--- virtual/src/main/java/com/example/virtual/Controller.java
+++ virtual/src/main/java/com/example/virtual/Controller.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
