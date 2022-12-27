d342121f701f57aee8cb03724dbbd6021fe4ac7e
joand <joandyalejogarcia@outlook.com>
2022-12-27 17:50:46 -0500

    + added logo to the application + added main.css file in the resources folder and connected it to the views.fxml file +added some basic styles to the overall project

--- virtual/src/main/java/com/example/virtual/Window.java
+++ virtual/src/main/java/com/example/virtual/Window.java
@@ -3,16 +3,20 @@ package com.example.virtual;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Window extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Window.class.getResource("views.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 380);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("main.css")).toExternalForm());
         stage.setTitle("J&E Virtual Chat");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Window.class.getResourceAsStream("logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
