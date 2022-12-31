02f3500eec89f3daaf12b253a21ec1fb8273853f
EdisonYCA <edisonyca127@gmail.com>
2022-12-31 13:57:16 -0500

    Create Server methods to send message to client GUI, and receive message from client and display to server GUI.

---
+++ virtual/src/main/java/com/example/virtual/Server.java
@@ -0,0 +1,54 @@
+package com.example.virtual;
+
+import javafx.scene.layout.VBox;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket; // accepts incoming requests to join server
+    private BufferedWriter bw; // write messages to client
+    private BufferedReader br; // read messages to client
+    private Socket socket; // communication end point for client
+    public Server(ServerSocket serverSocket){
+        try {
+            this.serverSocket = serverSocket;
+            socket = serverSocket.accept(); // blocking method to accept connection
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException io){
+            io.printStackTrace();
+            // close streams
+        }
+    }
+
+    // send message from sever -> client through socket
+    public void sendMessageToClient(String message){
+        try {
+            bw.write(message);
+            bw.newLine(); // indicate end of message
+            bw.flush();
+        }catch (IOException io){
+            io.printStackTrace();
+        }
+    }
+
+    // listen for messages from clients
+    public void receiveMessageFromClient(VBox messageDisplay){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(socket.isConnected()) {
+                    try {
+                        String messageFromClient = br.readLine(); // read messages when "\n" appears
+                        ServerController.displayMessageFromClient(messageFromClient, messageDisplay);
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        }).start();
+    }
+}
