77f9bb15811ce9ef88d89956aff5615fbbd5884d
EdisonYCA <edisonyca127@gmail.com>
2023-01-07 12:27:40 -0500

    create uploadImage method to allow user to set a profile picture

--- server/src/main/java/com/chat/virtual/login/LoginController.java
+++ server/src/main/java/com/chat/virtual/login/LoginController.java
@@ -2,15 +2,29 @@ package com.chat.virtual.login;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.jar.JarFile;
+
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class LoginController implements Initializable {
 
     @FXML
     private TextField usernameField;
+    @FXML
+    private ImageView profilePicture;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -52,6 +66,28 @@ public class LoginController implements Initializable {
     public void invalidUsernameAnimation(){
         usernameField.setStyle("-fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-border-color: #B21807");
         // add shake animation
+    }
+
+    // allows user to upload an image as a profile picture
+    public void uploadImage(){
+        JFileChooser file_upload = new JFileChooser();
+
+        // create file filter for only images
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpeg", "jpg", "gif");
+        file_upload.setFileFilter(filter);
+
+        int resVal = file_upload.showOpenDialog(null); // returns approved int if user selects a file
+
+        if(resVal == JFileChooser.APPROVE_OPTION){ // user has uploaded a file
+            try {
+                FileInputStream file = new FileInputStream(file_upload.getSelectedFile().getAbsolutePath());
+                Image image = new Image(file);
+                profilePicture.setImage(image); // set image view to picture
+            } catch(FileNotFoundException fileNotFoundException){
+                System.out.println("There was an error opening this file, ensure it hasn't been deleted.");
+                fileNotFoundException.printStackTrace();
+            }
+        }
     }
 
     // validates username and enters chat room depending socket connection
