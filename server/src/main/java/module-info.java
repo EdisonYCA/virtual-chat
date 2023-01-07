799c49043abc90fc9573d97f7d5bb5a9b5254ee1
joand <joandyalejogarcia@outlook.com>
2023-01-06 20:54:38 -0500

    no longer using 2 modules now only using 1

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.virtual {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.chat.virtual.server to javafx.fxml;
+    opens com.chat.virtual.client to javafx.fxml;
+    exports com.chat.virtual.client;
     exports com.chat.virtual.server;
+    opens com.chat.virtual.server to javafx.fxml;
 }
