e3afc4a1de2e56fbe83e9050f95c733b64ef00e9
EdisonYCA <edisonyca127@gmail.com>
2023-01-18 22:49:13 -0500

    put duplicate code into methods, added comments to code fragments, created function to set random username for user at start of program

--- server/src/main/java/com/chat/virtual/server/ServerController.java
+++ server/src/main/java/com/chat/virtual/server/ServerController.java
@@ -18,7 +18,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import org.apache.commons.io.FilenameUtils;
-
+import java.lang.Math;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -38,7 +38,9 @@ public class ServerController implements Initializable {
     @FXML
     private VBox messageDisplay; // aligns messages sent/received vertically GUI
     private Server server; // server
-    private Image pfpImg;
+    private Image pfpImg; // profile picture
+
+    private final String username = generateUsername(); // user's username
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) { // allows manipulation of FXML widgets
@@ -53,24 +55,12 @@ public class ServerController implements Initializable {
 
     // display message from client -> server
     public static void displayMessageFromClient(String message, VBox messageDisplay){
-        HBox messageContainer = new HBox(); // create HBox to store new message in
-        messageContainer.setAlignment(Pos.CENTER_LEFT);
-        messageContainer.setPadding(new Insets(5,5,5,5));
+        HBox messageContainer = styleMessageContainer(false);
 
-        Text sentMessage = new Text(message);
-        sentMessage.setFill(Color.BLACK);
+        Text msg = new Text(message);
+        msg.setFill(Color.BLACK);
 
-        // load font
-        Font font = new Font("/com.chat.virtual/fonts/SourceSansPro-Regular.ttf", 12);
-
-        TextFlow textFlow = new TextFlow(sentMessage);
-        textFlow.setStyle("-fx-background-color: #e1e1e1; " +
-                "-fx-background-radius: 20px;");
-        textFlow.setPadding(new Insets(5, 10, 5, 10));
-        textFlow.setTextAlignment(TextAlignment.CENTER);
-
-
-        messageContainer.getChildren().add(textFlow);
+        messageContainer.getChildren().add(styleMessage(msg, false));
 
         // ensure application thread is modifying GUI
         Platform.runLater(new Runnable() {
@@ -83,58 +73,75 @@ public class ServerController implements Initializable {
 
         // send message from server -> client and display message on server GUI
         @FXML
-        public void sendMessage(ActionEvent event){
-            // displaying message
-            String messageToSend = messageField.getText();
+        public void sendMessage(){
+            String messageToSend = messageField.getText(); // message entered
 
             if(!messageToSend.isEmpty()) { // ensure message to be sent isn't empty
                 messageField.clear();
-                HBox messageContainer = new HBox(); // create HBox to store new message in
-                messageContainer.setAlignment(Pos.CENTER_RIGHT);
-                messageContainer.setPadding(new Insets(5, 5, 5, 5)); // padding
 
-                HBox textContainer = new HBox();//controls the message and profile picture's horizontal layout
-                VBox profileMsg = new VBox();//controls the message and profile picture and username vertical layout
+                Text message = new Text(messageToSend);
+                message.setFill(Color.WHITE);
 
-                Text sentMessage = new Text(messageToSend);
-                sentMessage.setFill(Color.WHITE);
-
-                //dummy data
-                Circle pfp = new Circle(15,Color.DARKSEAGREEN);
-                pfp.setFill(new ImagePattern(pfpImg));
-                Circle userStatus = new Circle(4, Color.DARKOLIVEGREEN);
-                Text username = new Text("Money Man");
+                /* Set and style users profile picture, username, and online status */
+                Circle pfp = new Circle(15, Color.ALICEBLUE); // profile picture
+                Circle userStatus = new Circle(4, Color.DARKOLIVEGREEN); // green when online, red when offline
+                // setting username
+                Text username = new Text(this.username);
                 username.setFill(Color.WHITE);
+                // userInfo stores user and user status and aligns them properly
                 HBox userInfo = new HBox(username, userStatus);
                 userInfo.setAlignment(Pos.TOP_RIGHT);
                 HBox.setMargin(userStatus, new Insets(0,0,0,3));
 
-
-                TextFlow textFlow = new TextFlow(sentMessage); // wrap message in text flow to add styling
-                textFlow.setStyle("-fx-background-color: #7D52D9; " +
-                        "-fx-background-radius: 20px;");
-                textFlow.setPadding(new Insets(5, 10, 5, 10));
-                textFlow.setTextAlignment(TextAlignment.CENTER);
-
-                /*note: the order in which nodes are added matters*/
-
-                //this will contain a Hbox that will manage the message and profile img horizontal position.
-                textContainer.getChildren().addAll(textFlow, pfp);
+                /* displaying messages with proper alignment */
+                HBox textContainer = new HBox(); //controls the message and profile picture's horizontal alignment
+                textContainer.getChildren().addAll(styleMessage(message, true), pfp);
                 HBox.setMargin(pfp, new Insets(0, 0 ,0 ,5));
                 VBox.setMargin(textContainer, new Insets(5, 0, 0, 0));
 
-                //this will contain a Vbox that will manage (textContainer) and the username's vertical position.
+                /* display messages*/
+                VBox profileMsg = new VBox(); //controls textContainer and the username's vertical alignment
                 profileMsg.getChildren().addAll(userInfo,textContainer);
-
                 profileMsg.setAlignment(Pos.BOTTOM_RIGHT);
 
+                HBox messageContainer = styleMessageContainer(true);
                 messageContainer.getChildren().add(profileMsg);
                 messageDisplay.getChildren().add(messageContainer);
             }
-            // sending message
+
+            // send message
             server.sendMessageToClient(messageToSend);
     }
 
+    // styles the message container
+    public static HBox styleMessageContainer(boolean send){
+        HBox hbox = new HBox(); // create HBox to store new message in
+
+        if(send) hbox.setAlignment(Pos.CENTER_RIGHT);
+        else hbox.setAlignment(Pos.CENTER_LEFT);
+        hbox.setPadding(new Insets(5, 5, 5, 5));
+
+        return hbox;
+    }
+
+    // style the message to be sent
+    public static TextFlow styleMessage(Text msg, boolean send){
+        TextFlow textFlow = new TextFlow(msg);
+        if(send) {
+            textFlow.setStyle("-fx-background-color: #7D52D9; " +
+                    "-fx-background-radius: 20px;");
+        } else {
+            textFlow.setStyle("-fx-background-color: #e1e1e1; " +
+                    "-fx-background-radius: 20px;");
+        }
+
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        textFlow.setTextAlignment(TextAlignment.CENTER);
+
+        return textFlow;
+    }
+
+    // allows a user to choose a valid image and display it as there profile picture
     public void uploadImage() {
         new Thread(new Runnable() {
             @Override
@@ -175,5 +182,20 @@ public class ServerController implements Initializable {
             }
         }
         return false;
+    }
+
+    // returns a random username
+    public String generateUsername(){
+        String username = "user";
+
+        int max = 20000;
+        int min = 10000;
+        int range = max - min + 1;
+
+        // generate random numbers from 20,000 to 10,000
+        int rand = (int)(Math.random() * range) + min;
+        username += Integer.toString(rand);
+
+        return username;
     }
 }
