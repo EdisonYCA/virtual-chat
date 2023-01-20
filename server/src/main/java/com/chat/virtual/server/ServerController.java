eb002c0ba8d9573fd2e179429dadeeca79f0acaa
EdisonYCA <edisonyca127@gmail.com>
2023-01-20 18:18:49 -0500

    add JInputDialog

--- server/src/main/java/com/chat/virtual/server/ServerController.java
+++ server/src/main/java/com/chat/virtual/server/ServerController.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
